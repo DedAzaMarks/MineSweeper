802b5ab1495a05a3ccafeeedce6579713d2ea8b7
DedAzaMarks <34397798+DedAzaMarks@users.noreply.github.com>
2018-06-05 21:42:00 +0300

    Add files via upload
    Half of the project made on Java

---
+++ KotlinLearning/src/sweeper/Ranges.java
@@ -0,0 +1,32 @@
+package sweeper;
+
+import java.util.ArrayList;
+
+public class Ranges {
+    private static Coord size;
+    private static ArrayList<Coord> allCoords;
+
+
+    public static Coord getSize() {
+        return size;
+
+    }
+
+    static void setSize(Coord Size) {
+        size = Size;allCoords = new ArrayList<Coord>();
+        for (int y = 0; y < size.y; y++)
+            for (int x = 0; x < size.x; x++)
+                allCoords.add(new Coord(x, y));
+
+    }
+
+    public static ArrayList<Coord> getAllCoords () {
+        return allCoords;
+    }
+
+    static boolean inRange (Coord coord) {
+        return coord.x >= 0 && coord.x < size.x &&
+               coord.y >= 0 && coord.y < size.y;
+    }
+}
+
